fb8c6f3cbad31d93b818c0f20d2f203c4d212a3e
WhosYourDaddy42069 <82200662+Rimmen17@users.noreply.github.com>
2021-06-10 14:29:39 +0200

    Add files via upload

---
+++ If int print example.py
@@ -0,0 +1,7 @@
+print("Du vil gerne på bar")
+x = int(input(":Dørmand: Hvor gammel er du? "))
+
+if x>=18:
+    print("Kom du bare LaD, der massere damer")
+if x<18:
+    print("Øv bøv du må hjem og amme på far")
